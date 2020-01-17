22e5586873b582cd79fb2e441f4964a6fa046e66
Asja Makarowska <anutya@mail.ru>
2020-01-17 13:30:47 +0300

    add new checks (login url, login and register form)

--- pages/login_page.py
+++ pages/login_page.py
@@ -14,9 +14,10 @@ class LoginPage(BasePage):
         # реализуйте проверку на корректный url адрес
 
     def should_be_login_form(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_FORM), "Login form is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
         # реализуйте проверку, что есть форма логина
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        assert self.is_element_present(*MainPageLocators.REG_FORM), "Login form is not presented"
+        assert self.is_element_present(*LoginPageLocators.REG_FORM), "Login form is not presented"
+
